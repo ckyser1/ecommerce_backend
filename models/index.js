385517ee5667d5e5a4857356cffa7e2f40b51a57
ckyser1 <97637742+ckyser1@users.noreply.github.com>
2022-03-10 22:07:10 -0500

    added api routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: 'ProductTag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: 'ProductTag',
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,80 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk({
+      include: [{ model: Product }],
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+      res.status(200).json(catData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const locData = await Category.create({
+      category_id: req.body.category_id,
+    });
+    res.status(200).json(locData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{
+    const catdata = await Category.update({
+      where: {id: req.parms.id,},
+    });
+    
+  if (!catData) {
+    res.status(404).json({ message: 'No product found with that id!' });
+    return;
+  }
+    res.status(200).json(catData);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'No cat found with that id!' });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
